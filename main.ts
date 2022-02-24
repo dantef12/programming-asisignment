5030d496e9efd46c9edd8c5ffaa69a40e39f16c0
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-02-24 11:15:44 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    ball_count += 1
+    basic.showNumber(ball_count)
+})
+input.onButtonPressed(Button.B, function () {
+    strike_count += 1
+    basic.showNumber(strike_count)
+})
+let strike_count = 0
+let ball_count = 0
+ball_count = 0
+strike_count = 0
+basic.forever(function () {
+	
+})
