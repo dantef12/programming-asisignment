74c1d0ef8460f608efe1ae54ddb1b3f8cc9509d2
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-03-01 10:53:55 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,32 @@
 input.onButtonPressed(Button.A, function () {
     ball_count += 1
     basic.showNumber(ball_count)
+    basic.showString("b")
+    if (ball_count == 4) {
+        basic.showString("Walk")
+        basic.clearScreen()
+        ball_count = 0
+        soundExpression.yawn.playUntilDone()
+        basic.showNumber(ball_count)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showNumber(0)
 })
 input.onButtonPressed(Button.B, function () {
     strike_count += 1
     basic.showNumber(strike_count)
+    basic.showString("s")
+    if (strike_count == 3) {
+        basic.showString("Strikeout!")
+        soundExpression.soaring.playUntilDone()
+        basic.clearScreen()
+        strike_count = 0
+        basic.showNumber(strike_count)
+    }
 })
 let strike_count = 0
 let ball_count = 0
 ball_count = 0
 strike_count = 0
-basic.forever(function () {
-	
-})
